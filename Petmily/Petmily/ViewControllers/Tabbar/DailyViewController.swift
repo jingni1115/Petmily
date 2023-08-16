9aa02af8d09075effb91c79bb4418de1d9ec2d06
jingni1115 <kj227777@naver.com>
2023-08-16 18:29:37 +0900

    데일리 영상 스크롤 구현 완료

--- Petmily/Petmily/ViewControllers/Tabbar/DailyViewController.swift
+++ Petmily/Petmily/ViewControllers/Tabbar/DailyViewController.swift
@@ -7,23 +7,100 @@
 
 import UIKit
 
-class DailyViewController: BaseViewController {
+class DailyViewController: UIViewController {
 
+    /** @brief collectionView*/
+    @IBOutlet weak var cvMain: UICollectionView!
+    
+    /** @brief containerView 상단 마진*/
+    @IBOutlet weak var containerTopMargin: NSLayoutConstraint!
+    
+    /** @brief containerView 하단 마진*/
+    @IBOutlet weak var containerBottomMargin: NSLayoutConstraint!
+    
+    var isPlay = false
+    var isMute = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let layout = UICollectionViewFlowLayout.init()
+        cvMain.translatesAutoresizingMaskIntoConstraints = false
+        cvMain.showsVerticalScrollIndicator = false
+        layout.scrollDirection = .vertical
+        
+        
+        cvMain.contentInsetAdjustmentBehavior = .never
+        cvMain.setCollectionViewLayout(layout, animated: false)
+        cvMain.register(ReelCollectionViewCell.self, forCellWithReuseIdentifier: "ReelCollectionViewCell")
+        cvMain.isPagingEnabled = true
+        cvMain.backgroundColor = .systemBackground
+        cvMain.delegate = self
+        cvMain.dataSource = self
     }
+    
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension DailyViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        var reels : [ReelData] = [ReelData(video: "demo1"), ReelData(video: "demo2"), ReelData(video: "demo3"), ReelData(video: "demo4")]
+        return reels.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var reels : [ReelData] = [ReelData(video: "demo1"), ReelData(video: "demo2"), ReelData(video: "demo3"), ReelData(video: "demo4")]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReelCollectionViewCell", for: indexPath) as! ReelCollectionViewCell
+        cell.reelData = reels[indexPath.row]
+//        cell.delegate = self
+        if let urlPath = Bundle.main.url(forResource: reels[indexPath.row].video, withExtension: "mp4"){
+            cell.setUpPlayer(url: urlPath, bounds: collectionView.frame)
+            if !isPlay{
+                cell.avQueuePlayer?.play()
+                isPlay = true
+            }
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+       
+        
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        cvMain.visibleCells.forEach { cell in
+            let cell = cell as! ReelCollectionViewCell
+            cell.avQueuePlayer?.pause()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        cvMain.visibleCells.forEach { cell in
+            let cell = cell as! ReelCollectionViewCell
+            cell.avQueuePlayer?.seek(to: .zero)
+            cell.avQueuePlayer?.play()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let cell = cell as! ReelCollectionViewCell
+        cell.avQueuePlayer?.pause()
+    }
+    
+    
+    
 }
---
+++ Petmily/Petmily/ViewControllers/Tabbar/ReelCollectionViewCell.swift
@@ -0,0 +1,176 @@
+//
+//  ReelCollectionViewCell.swift
+//  Petmily
+//
+//  Created by 김지은 on 2023/08/16.
+//
+
+import UIKit
+import AVKit
+
+class ReelCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var vPlayer: UIView!
+    
+    var avQueuePlayer : AVQueuePlayer?
+    var avplayerLayer : AVPlayerLayer?
+    var isMuted = false
+    var pause = false
+
+        
+    var reelData : ReelData?
+    
+    let playerView : UIView = {
+        let pv = UIView()
+        pv.translatesAutoresizingMaskIntoConstraints = false
+        pv.isUserInteractionEnabled = true
+        return pv
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    // MARK: LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(playerView)
+//        addSubview(reelDetails)
+        setUpConstrains()
+        
+//        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressAction))
+//        addGestureRecognizer(longPress)
+        
+//        let doubletap = UITapGestureRecognizer(target: self, action: #selector(doubleTap(_:)))
+//        doubletap.numberOfTapsRequired = 2
+//        addGestureRecognizer(doubletap)
+//
+//        let onetap = UITapGestureRecognizer(target: self, action: #selector(muteTap(_:)))
+//        addGestureRecognizer(onetap)
+        
+        
+//        onetap.require(toFail: doubletap) // doubleTap doesn't effect on singleTap
+        
+//        reelDetails.moreButton.addTarget(self, action: #selector(showAlertSheet), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpPlayer(url:URL ,bounds:CGRect){
+        avQueuePlayer = AVQueuePlayer(url: url)
+        avplayerLayer = AVPlayerLayer(player: avQueuePlayer!)
+        avplayerLayer?.frame = bounds
+        avplayerLayer?.fillMode = .both
+        avplayerLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        playerView.layer.addSublayer(avplayerLayer!)
+    }
+    
+    func setUpConstrains(){
+        NSLayoutConstraint.activate([
+//            reelDetails.leadingAnchor.constraint(equalTo: leadingAnchor),
+//            reelDetails.trailingAnchor.constraint(equalTo: trailingAnchor),
+//            reelDetails.bottomAnchor.constraint(equalTo: bottomAnchor),
+//            reelDetails.heightAnchor.constraint(equalToConstant: 240),
+            
+            playerView.leadingAnchor.constraint(equalTo: playerView.leadingAnchor),
+            playerView.trailingAnchor.constraint(equalTo: playerView.trailingAnchor),
+            playerView.topAnchor.constraint(equalTo: playerView.topAnchor),
+            playerView.bottomAnchor.constraint(equalTo: playerView.bottomAnchor),
+        ])
+    }
+    
+    
+    
+    @objc func doubleTap(_ gesture : UIGestureRecognizer){
+        guard let gestureView = gesture.view else {return}
+        let size = gestureView.frame.width / 4
+        let heart = UIImageView(image: UIImage(named: "white-heart"))
+        heart.frame = CGRect(x: (gestureView.frame.width-size)/2,
+                             y: (gestureView.frame.height-size)/2,
+                             width: size ,
+                             height: size)
+        gestureView.addSubview(heart)
+//        self.reelData?.isLiked = true/
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .curveEaseIn) {
+            heart.transform = .init(scaleX: 1.5, y: 1.5)
+        } completion: { finished in
+            heart.transform = .identity
+            heart.removeFromSuperview()
+        }
+
+
+        
+    }
+    
+    @objc func muteTap(_ gesture:UIGestureRecognizer){
+        isMuted = !isMuted
+        guard let gestureView = gesture.view else {return}
+        let size = gestureView.frame.width / 4
+        let mute = UIImageView(image: UIImage(named: "mute"))
+        mute.tintColor = .white
+        mute.frame = CGRect(x: (gestureView.frame.width-size + 25)/2,
+                             y: (gestureView.frame.height-size + 25)/2,
+                             width: size - 25,
+                             height: size - 25)
+        
+        let unmute = UIImageView(image: UIImage(named: "unmute"))
+        unmute.tintColor = .white
+        unmute.frame = CGRect(x: (gestureView.frame.width-size + 25)/2,
+                             y: (gestureView.frame.height-size + 25)/2,
+                             width: size - 25,
+                             height: size - 25)
+                
+        if isMuted {
+            gestureView.addSubview(mute)
+            avQueuePlayer?.isMuted = true
+            UIView.animate(withDuration: 1) {
+                mute.alpha = 0
+            } completion: { done in
+                if done {mute.removeFromSuperview()
+                }
+            }
+            
+        }else {
+            gestureView.addSubview(unmute)
+            avQueuePlayer?.isMuted = false
+            
+            UIView.animate(withDuration: 1) {
+                unmute.alpha = 0
+            } completion: { done in
+                if done {unmute.removeFromSuperview()
+                }
+            }
+        }
+    }
+    
+//    @objc func longPressAction(_ gesture : UILongPressGestureRecognizer){
+//        if gesture.state == .began {
+//            avQueuePlayer?.pause()
+//            delegate?.hideWhenLongTouchBegan()
+//            UIView.animate(withDuration: 0.4){
+//                DispatchQueue.main.async {
+//                    self.reelDetails.alpha = 0
+//
+//                }
+//            }
+//        }
+//        if gesture.state == .ended {
+//            avQueuePlayer?.play()
+//            delegate?.showWhenLongTouchEnded()
+//            UIView.animate(withDuration: 0.4){
+//                DispatchQueue.main.async {
+//                    self.reelDetails.alpha = 1
+//
+//                }
+//            }
+//        }
+//    }
+    
+//    @objc func showAlertSheet(){
+//        delegate?.presentActionSheet()
+//    }
+}
