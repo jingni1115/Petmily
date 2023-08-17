cba3effb824fa56aed29b56c34b79fc1ebef12df
Soobean <saen5848@naver.com>
2023-08-17 20:11:18 +0900

    feat: AdoptViewController cell 8개 완성 cmbanner 배치 완료/ ContentsCollectionViewCell 생성

---
+++ Petmily/Petmily/Cell/ContentsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContentsCollectionViewCell.swift
+//  Petmily
+//
+//  Created by 허수빈 on 2023/08/17.
+//
+
+import UIKit
+
+class ContentsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
