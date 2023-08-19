da189baeb9dec3bd2107cc843ea9d476da518ca0
angwoo0503 <angwoo0503@gmail.com>
2023-08-20 05:41:34 +0900

    fix : 검색 기능 오류 수정

--- Petmily/Petmily/ViewControllers/Tabbar/InfoViewController.swift
+++ Petmily/Petmily/ViewControllers/Tabbar/InfoViewController.swift
@@ -15,7 +15,7 @@ class InfoViewController: BaseViewController, UISearchBarDelegate {
     // 검색중 여부
     var isSearching = false
     // 검색 결과를 저장할 배열
-    var filteredInfoList = [Info]()
+    var filteredInfoList = [InfoModel]()
     
     var infoData: [InfoModel]?
     var userData: UserModel?
@@ -26,15 +26,19 @@ class InfoViewController: BaseViewController, UISearchBarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tbvInfo.reloadData()
-        setUI()
+        
         getInfoData()
+        setUI()
+        
+        tbvInfo.reloadData()
     }
     
     func getInfoData() {
         FirestoreService().fetchInfoData { result in
             self.infoData = result
+            self.filteredInfoList = self.infoData ?? []
             print(result)
+            print("겟 인포 데이터 : \(result.count)")
             self.getUserData()
         }
     }
@@ -59,51 +63,48 @@ class InfoViewController: BaseViewController, UISearchBarDelegate {
         infoSearchBar.delegate = self
         infoSearchBar.backgroundImage = UIImage()
         infoSearchBar.placeholder = "원하시는 동물 종을 검색해보세요"
-        filteredInfoList = InfoList.list
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.trimmingCharacters(in: .whitespaces).isEmpty{
-            // 검색어가 비어있으면 전체 리스트를 보여줌
+        if searchText.isEmpty {
             isSearching = false
-            filteredInfoList = InfoList.list
-        } else
-        if searchText.count == 1 {
-            isSearching = true
-            // 검색어가 1개인 경우 아무 동작도 하지 않음
             filteredInfoList = []
         } else {
             isSearching = true
-            filteredInfoList = InfoList.list.filter { info in
+            filteredInfoList = infoData?.filter { info in
                 let lowercasedSearchText = searchText.lowercased()
-                
-                // 제목, 본문, 해시태그 중 하나라도 일치하는지 확인
-                let titleMatches = info.title.lowercased().contains(lowercasedSearchText)
-                let contentMatches = info.description.lowercased().contains(lowercasedSearchText)
-                let hashtagMatches = info.tag.lowercased().contains(lowercasedSearchText)
-                
-                return titleMatches || contentMatches || hashtagMatches
-            }
+                let titleMatches = info.title?.lowercased().contains(lowercasedSearchText) ?? false
+                let contentMatches = info.content?.lowercased().contains(lowercasedSearchText) ?? false
+                return titleMatches || contentMatches
+            } ?? []
         }
         tbvInfo.reloadData()
     }
-    
-    
 }
 
 
 
 
+
 extension InfoViewController: UITableViewDelegate, UITableViewDataSource {
+    
     // 출력할 셀의 개수
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return infoData?.count ?? 0
+        if isSearching {
+            return filteredInfoList.count
+        } else {
+            return infoData?.count ?? 0
+        }
     }
     
     // 셀 설정
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "InfoTableViewCell", for: indexPath) as! InfoTableViewCell
-        cell.setInfo(info: infoData?[indexPath.row])
+        if isSearching {
+            cell.setInfo(info: filteredInfoList[indexPath.row])
+        } else {
+            cell.setInfo(info: infoData?[indexPath.row])
+        }
         return cell
     }
     
@@ -112,9 +113,8 @@ extension InfoViewController: UITableViewDelegate, UITableViewDataSource {
         tbvInfo.deselectRow(at: indexPath, animated: true)
         
         // 선택한 정보 가져오기
-        let selectedInfo = infoData?[indexPath.row]
+        let selectedInfo = filteredInfoList[indexPath.row]
         let selectedUser = userData
-        
         
         // 목표 뷰 컨트롤러 초기화
         let vc = InfoDetailViewController.init(nibName: "InfoDetailViewController", bundle: nil)
@@ -122,60 +122,17 @@ extension InfoViewController: UITableViewDelegate, UITableViewDataSource {
         vc.selectedUser = selectedUser // 유저 정보
         navigationPushController(viewController: vc, animated: true)
     }
-
-
-    
-    public override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        // 스크롤 뷰의 현재 Y 오프셋
-        let offsetY = scrollView.contentOffset.y
-        
-        // 사용자가 화면을 위로 스크롤할 때만 데이터 로딩
-        if offsetY < -20 {
-            // 데이터 로딩 함수 호출
-            loadData()
-        }
-    }
-    
-    // 사용자가 화면을 위로 스크롤할 때 데이터를 더 로드하는 함수
-    func loadData() {
-        // 데이터 로딩 중이거나 추가로 불러올 데이터가 없으면 종료
-        guard !isLoadingData && isMoreDataAvailable else {
-            return
-        }
-        
-        // 검색중이라면 종료
-        guard !isSearching else {
-            return
-        }
-        
-        // 데이터 로딩 중임을 표시
-        isLoadingData = true
-        
-        // 실제 데이터 로딩 로직
-        DispatchQueue.global().async { [weak self] in
-            // 새 데이터를 로드
-            let newData = InfoList.loadMoreData(page: self?.currentPage ?? 1, itemsPerPage: self?.itemsPerPage ?? 10)
-            let newUserData = UserList.loadMoreUserData(page: self?.currentPage ?? 1, itemsPerPage: self?.itemsPerPage ?? 10)
-            
-            DispatchQueue.main.async {
-                // 로드된 데이터가 없으면 추가 데이터 없음으로 표시
-                if newData.isEmpty || newUserData.isEmpty {
-                    self?.isMoreDataAvailable = false
-                } else {
-                    // 로드된 데이터를 배열의 맨 앞에 추가
-                    InfoList.list.insert(contentsOf: newData, at: 0)
-                    UserList.list.insert(contentsOf: newUserData, at: 0)
-                    // 현재 페이지를 증가시킴
-                    self?.currentPage += 1
-                    // 필터된 데이터도 업데이트
-                    self?.filteredInfoList.insert(contentsOf: newData, at: 0)
-                }
-                
-                // 데이터 로딩이 완료되었으므로 상태를 업데이트
-                self?.isLoadingData = false
-                // 테이블 뷰를 다시 로드하여 변경사항을 반영
-                self?.tbvInfo.reloadData()
-            }
-        }
-    }
 }
+
+
+//
+//    public override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+//        // 스크롤 뷰의 현재 Y 오프셋
+//        let offsetY = scrollView.contentOffset.y
+//
+//        // 사용자가 화면을 위로 스크롤할 때만 데이터 로딩
+//        if offsetY < -20 {
+//            // 데이터 로딩 함수 호출
+//            loadData()
+//        }
+//    }
