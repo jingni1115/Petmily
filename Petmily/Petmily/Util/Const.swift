d308d1bb5ce582a061f12358788dd9dd8d28d8fc
p2glet <vviian.2@gmail.com>
2023-09-14 23:23:10 +0900

    [FEAT] : setting Components and added SnapKit

---
+++ Petmily/Petmily/Util/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Petmily
+//
+//  Created by t2023-m0056 on 2023/09/14.
+//
+
+import UIKit
+
+public enum Font {
+    static let myPageTitleFont = UIFont.systemFont(ofSize: 20)
+    static let myPageLabelFont = UIFont.boldSystemFont(ofSize: 16)
+}
