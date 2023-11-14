dc60e80a2744a20f23f7fb854ffe1558a3593fe4
jingni1115 <kj227777@naver.com>
2023-11-14 11:22:52 +0900

    [FEAT]: daily 사진 촬영 기능 -> 비디오 촬영 기능으로 변경

--- Petmily/Petmily/ViewControllers/Tabbar/CameraViewController.swift
+++ Petmily/Petmily/ViewControllers/Tabbar/CameraViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+class CameraViewController: UIViewController {
+    
+    
     // http://yoonbumtae.com/?p=5762
     @IBOutlet weak var btnX: UIButton!
     @IBOutlet weak var btnFlash: UIButton!
@@ -19,21 +21,24 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     var videoPreviewLayer: AVCaptureVideoPreviewLayer!
     var stillImageOutput: AVCapturePhotoOutput!
     
+    var movieFileOutput: AVCaptureMovieFileOutput!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         captureSession = AVCaptureSession()
         captureSession.beginConfiguration()
-
+        
         guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
-
+        
         do {
             let cameraInput = try AVCaptureDeviceInput(device: captureDevice)
             
-            stillImageOutput = AVCapturePhotoOutput()
-
+            movieFileOutput = AVCaptureMovieFileOutput()
+            
             captureSession.addInput(cameraInput)
-            captureSession.sessionPreset = .photo
-            captureSession.addOutput(stillImageOutput)
+            captureSession.sessionPreset = .hd1280x720
+            captureSession.addOutput(movieFileOutput)
             captureSession.commitConfiguration()
         } catch {
             print(error)
@@ -45,63 +50,75 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         }
         videoPreviewLayer?.videoGravity = .resizeAspectFill
         self.vPreview.layer.addSublayer(videoPreviewLayer)
-
-        captureSession.startRunning()
+        DispatchQueue.global(qos: .background).async {            self.captureSession.startRunning()
+        }
         // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-//        setupAndStartCaptureSession()
+    func startRecording() {
+        let outputFileName = NSUUID().uuidString
+        let outputFilePath = (NSTemporaryDirectory() as NSString).appendingPathComponent(outputFileName).appending(".mov")
+        let outputURL = URL(fileURLWithPath: outputFilePath)
+        
+        movieFileOutput.startRecording(to: outputURL, recordingDelegate: self)
     }
     
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let imageData = photo.fileDataRepresentation()
-            else { return }
-        
-        let image = UIImage(data: imageData)
-        img.image = image
+    func stopRecording() {
+        movieFileOutput.stopRecording()
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //        setupAndStartCaptureSession()
     }
     
     private func switchCamera(captureSession: AVCaptureSession?) {
         captureSession?.beginConfiguration()
         let currentInput = captureSession?.inputs.first as? AVCaptureDeviceInput
         captureSession?.removeInput(currentInput!)
-
+        
         let newCameraDevice = currentInput?.device.position == .back ? camera(with: .front) : camera(with: .back)
         let newVideoInput = try? AVCaptureDeviceInput(device: newCameraDevice!)
         captureSession?.addInput(newVideoInput!)
         captureSession?.commitConfiguration()
     }
-
+    
     private func camera(with position: AVCaptureDevice.Position) -> AVCaptureDevice? {
         let devices = AVCaptureDevice.devices(for: AVMediaType.video)
         return devices.filter { $0.position == position }.first
     }
     
-    func didTapBtnTakePicture(stillImageOutput: AVCapturePhotoOutput) {
-        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
-//        settings.flashMode = flashMode.value == .off ? .off : .on
-        stillImageOutput.capturePhoto(with: settings, delegate: self)
-    }
+    //    func didTapBtnTakePicture(stillImageOutput: AVCaptureMovieFileOutput) {
+    ////        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.h264])
+    ////        settings.flashMode = flashMode.value == .off ? .off : .on
+    //        stillImageOutput.startRecording(to: settings, recordingDelegate: self)
+    //    }
     
     @IBAction func gearButtonTouched(_ sender: Any) {
         
     }
     
     @IBAction func cameraButtonTouched(_ sender: Any) {
-        stillImageOutput?.capturePhoto(with: AVCapturePhotoSettings(), delegate: self as AVCapturePhotoCaptureDelegate)
+        if !movieFileOutput.isRecording {
+            startRecording()
+        } else {
+            stopRecording()
+        }
+        //        didTapBtnTakePicture(stillImageOutput: stillImageOutput)
+        //        stillImageOutput?.capturePhoto(with: AVCapturePhotoSettings(), delegate: self as AVCapturePhotoCaptureDelegate)
+        
     }
     
     @IBAction func flashButtonTouched(_ sender: UIButton) {
         //카메라 디바이스 가져오기
         guard let device = AVCaptureDevice.default(for: .video) else { return }
-         
+        
         //플래시를 지원한다면
         if device.hasTorch {
             do {
                 try device.lockForConfiguration()
-         
+                
                 //현재 off상태이면 on시키기
                 if device.torchMode == .off {
                     device.torchMode = .on
@@ -114,7 +131,7 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
                     //플래시 버튼 이미지 변경
                     sender.setImage(UIImage.init(systemName: "lightbulb.slash"), for: .normal)
                 }
-         
+                
                 device.unlockForConfiguration()
             } catch {
                 print("Torch could not be used")
@@ -122,7 +139,7 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         } else {
             print("Torch is not available")
         }
-//        didTapBtnTakePicture(stillImageOutput: stillImageOutput)
+        //        didTapBtnTakePicture(stillImageOutput: stillImageOutput)
     }
     
     @IBAction func conversionCameraButtonTouched(_ sender: Any) {
@@ -131,5 +148,18 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     
     @IBAction func xButtonTouched(_ sender: Any) {
         self.dismiss(animated: true)
+    }
+}
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        // 비디오 촬영이 시작됐을 때의 동작
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error recording video: \(error.localizedDescription)")
+        } else {
+            UISaveVideoAtPathToSavedPhotosAlbum(outputFileURL.path, nil, nil, nil)
+        }
     }
 }
